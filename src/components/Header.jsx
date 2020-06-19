fc16f73bc83e6ceb629f5f7655593d212e2b109a
KompAc-ua <nsvsergey18@gmail.com>
2020-06-19 17:43:11 +0300

    Skelet created

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://www.freelogodesign.org/Content/img/logo-samples/bakary.png"></img>
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://theimageconference.org/wp-content/uploads/2020/04/vancouver_image_conference_3-scaled.jpg"></img>
+            </div>
+            <div>ava+description</div>
+            <div>
+                my posts
+          <div>new post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Profile;
